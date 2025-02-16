74f40e90baa0486ed77e8ea31211fe8852fc4307
sasmita-stha <shresthasasmita7@gmail.com>
2025-02-16 20:46:36 +0545

    Add files via upload

---
+++ arithmetic_leftshift.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int binaryToDecimal(char binary[]) 
+{
+    int decimal = 0, len = strlen(binary);
+    for (int i = 0; i < len; i++) {
+        decimal = decimal * 2 + (binary[i] - '0');
+    }
+    return decimal;
+}
+
+void decimalToBinary(int num, int bits) 
+{
+    for (int i = bits - 1; i >= 0; i--) {
+        printf("%d", (num >> i) & 1);
+    }
+}
+
+int main() 
+{
+    char binary[20];
+    int shift;
+
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    printf("Enter shift count: ");
+    scanf("%d", &shift);
+
+    int decimal = binaryToDecimal(binary);
+    int bit_length = strlen(binary);  
+    int shifted = decimal << shift;
+
+    printf("Result after arithmetic left shift: ");
+    decimalToBinary(shifted, bit_length);
+    printf("\n");
+    return 0;
+}
+
